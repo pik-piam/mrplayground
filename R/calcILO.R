bce20b00a803d0f54b285f9f91589b8f4683ce12
amnmalik <amalik@pik-potsdam.de>
2020-10-11 19:39:16 +0200

    added ILO data on employment in mining and lignite and oil and gas extraction

---
+++ R/calcILO.R
@@ -0,0 +1,29 @@
+calcILO <- function(x,subtype){
+  x <- readSource("ILO")
+  x[is.na(x)] <- 0
+  
+  if(subtype=="Reliable")
+  {
+    x <- x[,,"Unreliable",pmatch=T,invert=T]
+    x[,,"Oil and Gas. "] <- x[,,"Oil and Gas. "]+x[,,"Coal and Lignite.Break in series"]
+    x[,,"Coal and Lignite. "] <- x[,,"Coal and Lignite. "]+x[,,"Coal and Lignite.Break in series"]
+    x <- x[,,c("Oil and Gas. ","Coal and Lignite. ")]
+    x <- collapseNames(x)
+    x <- magpiesort(x)
+    
+  }
+  
+  if(subtype=="all"){ # containts both unreliable and "break in series" labels
+    x[,,"Oil and Gas. "] <- dimSums(x[,,"Oil and Gas",pmatch=T])
+    x[,,"Coal and Lignite. "] <- dimSums(x[,,"Coal and Lignite",pmatch=T])
+    x <- x[,,c("Oil and Gas. ","Coal and Lignite. ")]
+    x <- collapseNames(x)
+    x <- magpiesort(x)
+  }
+  
+  return(list(
+    x=x,
+    weight=NULL,
+    unit="000s",
+    description="Employment in coal and lignite or oil and gas"))
+}
